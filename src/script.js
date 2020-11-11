7617d4a4f9d38e221c1f879e4e360fe7f396a7bd
AlekseyDanchin <aleksey.danchin@gmail.com>
2020-11-11 18:09:25 +0300

    day_02

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 const input = document.querySelector("input");
 const letters = Array.from(document.querySelectorAll("[data-letters]"));
 const specs = Array.from(document.querySelectorAll("[data-spec]"));
+const textExample = document.querySelector("#textExample");
 
-const text = `В начале XIII в. в Центральной Азии возникло новое государство - Монгольская империя. Объединение монгольских племен в немалой степени было вызвано изменением климатических условий местности, где проживали монголы. XI и XII вв. были благоприятными для монголов. Длительный период влажных лет в восточной степи привел к тому, что умножились стада, а, следовательно, одна и та же территория могла прокормить больше людей. Произошло увеличение населения в Монголии. Однако в конце XII в. климат стал постепенно меняться в сторону ухудшения, стал более засушливым. Кочевое скотоводство стало малопродуктивным, в степи стало много избыточного населения. Началась обычная в таких условиях борьба с соседями за пастбища, а также вторжения на земли соседей-земледельцев.
+const text = `В начале XIII в. в Центральной Азии возникло новое государство - Монгольская империя.
+Объединение монгольских племен в немалой степени было вызвано изменением климатических условий местности, где проживали монголы. XI и XII вв. были благоприятными для монголов. Длительный период влажных лет в восточной степи привел к тому, что умножились стада, а, следовательно, одна и та же территория могла прокормить больше людей. Произошло увеличение населения в Монголии. Однако в конце XII в. климат стал постепенно меняться в сторону ухудшения, стал более засушливым. Кочевое скотоводство стало малопродуктивным, в степи стало много избыточного населения. Началась обычная в таких условиях борьба с соседями за пастбища, а также вторжения на земли соседей-земледельцев.
 Чингисхан.Огромную роль в создании Монгольской империи сыграл Темуджин. Железная воля, властолюбие и целеустремленность позволили Темуджину собрать монголов в единое политическое целое и спаять общество на основе т. н. закона Чингисхана - Яссы. На съезде монгольской знати (курултае) в 1206 г. создатель империи Темуджин (Темучин) был провозглашен великим ханом всех монголов и принял имя Чингисхан. Монгольская империя оказалось исключительно боеспособной и жизнеспособной. Чингисхан силой объединил под своей рукой всех монголов, некоторые соседние племена и на основе родового признака создал войско, которому в XII - XIII вв., в среднеазиатских государствах, на Руси и в Европе равных не было. В последующие годы монгольские отряды подчинили себе окрестные народы, беспощадно уничтожая всех непокорных. В середине 20-х гг. XIII в. в состав монгольского государства вошли Северный Китай, Средняя Азия, Северная Индия.
 Рядовой единицей монгольского войска была десятка - семья, ближайшие родственники одной юрты, одного аила. Потом следовала сотня, в нее входили люди одного рода. Тысяча могла объединять два или три аила, далее шла тьма - десятитысячный отряд. В войске Чингисхана действовала Ясса: если в бою кто-то из десятки побежит от врага, то казнили всю десятку; если в сотне побежит десятка, то казнили всю сотню, если побежит сотня и откроет брешь врагу, то казнили всю тысячу. Основную ударную силу монголов составляла конница. Каждый воин имел два-три лука, веревочный аркан, топор, хорошо владел саблей. Лошадь воина покрывалась шкурами для защиты. Голову, шею и грудь воина защищали железный или медный шлем, панцирь из кожи. Монгольская конница на своих низкорослых выносливых конях могла проходить в сутки до 80 км, а с обозами, стенобитными, огнеметными орудиями - до 10 км.
 К 1211 г. монголы завоевали землю бурят, якутов, киргизов и уйгуров, т.е. подчинили себе практически все основные племена и народы Сибири, обложив их данью. В 1211 г. Чингисхан приступил к завоеванию северного Китая, которое было завершено лишь к 1234 г. Монголы в процессе завоевания заимствовали у китайцев различную военную технику, а также научились осаждать крепости при помощи стенобитных и осадных машин. Китайцы передали монголам и важную науку дипломатии. Монголы одними из первых стали придерживаться принципа неприкосновенности послов, беспощадно карая народы за нарушение этого принципа.Схватка грузин с монголо-татарами.
@@ -11,13 +13,14 @@ const text = `В начале XIII в. в Центральной Азии воз
 Еще при жизни Чингисхан разделил огромную империю между сыновьями на улусы, которые были в составе единого государства еще 40 лет после его смерти (1227 г.). Улус Угедэя - собственно Монголия и Северный Китай, улус Чагатая - Средняя Азия, улус Джучи - пространства к западу и югу от Иртыша до Уральских гор, Аральского и Каспийского морей. В 40-х гг. XIII в. выделился еще один улус, охватывающий часть Ирана и Закавказье, который был отдан внуку Чингисхана Хулагу. Далее процесс распада великой империи ускорился. В начале XIV в. улус Джучи распался на Синюю и Белую Орду. Впоследствии за Белой Ордой, располагавшейся в бассейне рек Волги и Дона, в Крыму и на Северном Кавказе, закрепилось название Золотой Орды.`;
 
 const party = createParty(text);
-console.log(party);
 
 init();
 
 function init() {
 	input.addEventListener("keydown", keydownHandler);
 	input.addEventListener("keyup", keyupHandler);
+
+	viewUpdate();
 }
 
 function keydownHandler(event) {
@@ -27,6 +30,7 @@ function keydownHandler(event) {
 
 	if (letter) {
 		letter.classList.add("pressed");
+		press(event.key);
 		return;
 	}
 
@@ -34,6 +38,11 @@ function keydownHandler(event) {
 
 	if (key === " ") {
 		key = "space";
+		press(" ");
+	}
+
+	if (key === "enter") {
+		press("\n");
 	}
 
 	const ownSpecs = specs.filter((x) => x.dataset.spec === key);
@@ -53,6 +62,7 @@ function keyupHandler(event) {
 
 	if (letter) {
 		letter.classList.remove("pressed");
+
 		return;
 	}
 
@@ -77,8 +87,8 @@ function createParty(text) {
 		maxStringLength: 70,
 		maxShowStrings: 3,
 		currentStringIndex: 0,
-		currentPrintedIndex: 0,
-		erros: [],
+		currentPressedIndex: 0,
+		errors: [],
 	};
 
 	party.text = party.text.replace(/\n/g, "\n ");
@@ -107,4 +117,97 @@ function createParty(text) {
 	}
 
 	return party;
+}
+
+function press(letter) {
+	const string = party.strings[party.currentStringIndex];
+	const mustLetter = string[party.currentPressedIndex];
+
+	if (letter === mustLetter) {
+		party.currentPressedIndex++;
+
+		if (string.length <= party.currentPressedIndex) {
+			party.currentPressedIndex = 0;
+			party.currentStringIndex++;
+		}
+	} else if (!party.errors.includes(mustLetter)) {
+		party.errors.push(mustLetter);
+	}
+
+	viewUpdate();
+}
+
+function viewUpdate() {
+	const string = party.strings[party.currentStringIndex];
+
+	const showedStrings = party.strings.slice(
+		party.currentStringIndex,
+		party.currentStringIndex + party.maxShowStrings
+	);
+
+	const div = document.createElement("div");
+
+	const firstLine = document.createElement("div");
+	firstLine.classList.add("line");
+	div.append(firstLine);
+
+	const done = document.createElement("span");
+	done.classList.add("done");
+	done.textContent = string.slice(0, party.currentPressedIndex);
+	firstLine.append(
+		done,
+		...string
+			.slice(party.currentPressedIndex)
+			.split("")
+			.map((letter) => {
+				if (letter === " ") {
+					return "·";
+				}
+
+				if (letter === "\n") {
+					return "¶";
+				}
+
+				if (party.errors.includes(letter)) {
+					const errorSpan = document.createElement("span");
+					errorSpan.classList.add("hint");
+					errorSpan.textContent = letter;
+					return errorSpan;
+				}
+
+				return letter;
+			})
+	);
+
+	for (let i = 1; i < showedStrings.length; i++) {
+		const line = document.createElement("div");
+		line.classList.add("line");
+		div.append(line);
+
+		line.append(
+			...showedStrings[i].split("").map((letter) => {
+				if (letter === " ") {
+					return "·";
+				}
+
+				if (letter === "\n") {
+					return "¶";
+				}
+
+				if (party.errors.includes(letter)) {
+					const errorSpan = document.createElement("span");
+					errorSpan.classList.add("hint");
+					errorSpan.textContent = letter;
+					return errorSpan;
+				}
+
+				return letter;
+			})
+		);
+	}
+
+	textExample.innerHTML = "";
+	textExample.append(div);
+
+	input.value = string.slice(0, party.currentPressedIndex);
 }
